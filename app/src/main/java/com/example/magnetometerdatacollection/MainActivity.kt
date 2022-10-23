86c520f4e3b2574f06b540e0795cac64ab432eaa
MilenkoO1997 <93483397+MilenkoO1997@users.noreply.github.com>
2022-10-23 14:16:32 +0200

    try

--- app/src/main/java/com/example/magnetometerdatacollection/MainActivity.kt
+++ app/src/main/java/com/example/magnetometerdatacollection/MainActivity.kt
@@ -1,33 +1,57 @@
 package com.example.magnetometerdatacollection
 
+import android.content.ContentValues.TAG
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.isInvisible
+import com.aware.Accelerometer
+//import com.aware.Accelerometer
+import com.aware.Aware
+import com.aware.Aware_Preferences
+import com.aware.providers.Accelerometer_Provider
+//import com.aware.providers.Accelerometer_Provider
 import com.bumptech.glide.Glide
-import com.bumptech.glide.load.resource.gif.GifDrawable
 
-//import pl.droidsonroids.gif.GifImageView
 
+@Suppress("UNREACHABLE_CODE")
 class MainActivity : AppCompatActivity() {
-//    val gifImageView = GifImageView(this)
-//    val StatImg : GifImageView = findViewById(R.id.gifImageView)
-//    val startBtn: Button = findViewById(R.id.button) as Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Aware.startAWARE(this) //initialise core AWARE service
+        //sampling frequency in microseconds
+        Aware.setSetting(this, Aware_Preferences.FREQUENCY_ACCELEROMETER,
+            200000)
+        // intensity threshold to report the reading
+        Aware.setSetting(this, Aware_Preferences.THRESHOLD_ACCELEROMETER,
+            0.02f)
+
         val imageView: ImageView = findViewById(R.id.imageView)
 //        Glide.with(this).load(R.drawable.gears2).into(imageView)
 //        Glide.with(this).asBitmap().load(R.drawable.gears2).into(imageView)
+
         Glide.with(this).asGif().load(R.drawable.gears2).into(imageView)
-        imageView.setVisibility(View.INVISIBLE);
+        imageView.setVisibility(View.INVISIBLE)
+
+        Accelerometer.setSensorObserver {
+        val x = it.getAsDouble(Accelerometer_Provider.Accelerometer_Data.VALUES_0)
+        val y = it.getAsDouble(Accelerometer_Provider.Accelerometer_Data.VALUES_1)
+        val z = it.getAsDouble(Accelerometer_Provider.Accelerometer_Data.VALUES_2)
+        runOnUiThread(java.lang.Runnable {
+            Log.i(TAG, "X:"+x.toString()+" Y:"+ y.toString()+" Z:" + z.toString())
+        })
+    }
     }
 
     override fun onResume() {
         super.onResume()
+        Aware.startAccelerometer(this)
         val startBtn: Button = findViewById(R.id.button) as Button
         val textView2: TextView = findViewById(R.id.textView2)
         startBtn.setOnClickListener{
@@ -35,7 +59,24 @@ class MainActivity : AppCompatActivity() {
             imageView . setVisibility (View.VISIBLE)
             textView2.text = "Collecting..."
         }
+//        try {
+//            Accelerometer.getSensorObserver()
+//            Toast.makeText(this, "Pass", Toast.LENGTH_SHORT).show()
+//        }
+//        catch (e: Exception)
+//        {
+//            Toast.makeText(this, "Crash", Toast.LENGTH_SHORT).show()
+//        }
     }
+
+    override fun onPause() {
+        super.onPause()
+        Aware.stopAccelerometer(this)
+    }
+
+//    fun collecting(){
+//
+//    }
 }
 
 
