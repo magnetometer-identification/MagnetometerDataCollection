689e4bd384530407220121a7646df110042c7ba2
MilenkoO1997 <93483397+MilenkoO1997@users.noreply.github.com>
2022-10-23 22:49:59 +0200

    Missing: -finish 5 stages protocol -data writing to file -connection to server -problem of Indexing with unique ID (may need server for that)

--- app/src/main/java/com/example/magnetometerdatacollection/MainActivity.kt
+++ app/src/main/java/com/example/magnetometerdatacollection/MainActivity.kt
@@ -1,82 +1,107 @@
 package com.example.magnetometerdatacollection
 
-import android.content.ContentValues.TAG
+import android.app.AlertDialog
+import android.app.Dialog
+import android.content.DialogInterface
+import android.content.Intent
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.TextView
-import android.widget.Toast
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
-import com.aware.Accelerometer
-//import com.aware.Accelerometer
+import androidx.fragment.app.DialogFragment
 import com.aware.Aware
-import com.aware.Aware_Preferences
-import com.aware.providers.Accelerometer_Provider
-//import com.aware.providers.Accelerometer_Provider
+import com.aware.Magnetometer
+import com.aware.providers.Magnetometer_Provider
 import com.bumptech.glide.Glide
+import java.util.*
+import kotlin.concurrent.schedule
 
 
 @Suppress("UNREACHABLE_CODE")
 class MainActivity : AppCompatActivity() {
+
+    lateinit var startBtn: Button
+    lateinit var  textView2: TextView
+    lateinit var imageView: ImageView
+    lateinit var UserIDfield: EditText
+
+    lateinit var deviceID: String
+    lateinit var UserID: String
+
+
+    var done: Boolean = false
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        Aware.startAWARE(this) //initialise core AWARE service
-        //sampling frequency in microseconds
-        Aware.setSetting(this, Aware_Preferences.FREQUENCY_ACCELEROMETER,
-            200000)
-        // intensity threshold to report the reading
-        Aware.setSetting(this, Aware_Preferences.THRESHOLD_ACCELEROMETER,
-            0.02f)
+        startActivity(Intent(this@MainActivity, PhaseActivity::class.java))
 
-        val imageView: ImageView = findViewById(R.id.imageView)
+        startBtn = findViewById(R.id.button)
+        textView2 = findViewById(R.id.textView2)
+        imageView = findViewById(R.id.imageView)
+        UserIDfield = findViewById(R.id.UserIDfield)
+
+
+        Aware.startAWARE(applicationContext)
+
+        Magnetometer.setSensorObserver {
+            val x = it.getAsDouble(Magnetometer_Provider.Magnetometer_Data.VALUES_0)
+            val y = it.getAsDouble(Magnetometer_Provider.Magnetometer_Data.VALUES_1)
+            val z = it.getAsDouble(Magnetometer_Provider.Magnetometer_Data.VALUES_2)
+            println("x = $x y = $y, z = $z")
+        }
 //        Glide.with(this).load(R.drawable.gears2).into(imageView)
 //        Glide.with(this).asBitmap().load(R.drawable.gears2).into(imageView)
 
-        Glide.with(this).asGif().load(R.drawable.gears2).into(imageView)
-        imageView.setVisibility(View.INVISIBLE)
+//        Glide.with(this).asGif().load(R.drawable.gears2).into(imageView)
+//        imageView.visibility = View.INVISIBLE
 
-        Accelerometer.setSensorObserver {
-        val x = it.getAsDouble(Accelerometer_Provider.Accelerometer_Data.VALUES_0)
-        val y = it.getAsDouble(Accelerometer_Provider.Accelerometer_Data.VALUES_1)
-        val z = it.getAsDouble(Accelerometer_Provider.Accelerometer_Data.VALUES_2)
-        runOnUiThread(java.lang.Runnable {
-            Log.i(TAG, "X:"+x.toString()+" Y:"+ y.toString()+" Z:" + z.toString())
-        })
-    }
     }
 
     override fun onResume() {
         super.onResume()
-        Aware.startAccelerometer(this)
-        val startBtn: Button = findViewById(R.id.button) as Button
-        val textView2: TextView = findViewById(R.id.textView2)
-        startBtn.setOnClickListener{
-            val imageView = this.findViewById<ImageView>(R.id.imageView)
-            imageView . setVisibility (View.VISIBLE)
-            textView2.text = "Collecting..."
-        }
-//        try {
-//            Accelerometer.getSensorObserver()
-//            Toast.makeText(this, "Pass", Toast.LENGTH_SHORT).show()
-//        }
-//        catch (e: Exception)
-//        {
-//            Toast.makeText(this, "Crash", Toast.LENGTH_SHORT).show()
-//        }
+        collecting()
     }
 
     override fun onPause() {
         super.onPause()
-        Aware.stopAccelerometer(this)
+        Aware.stopMagnetometer(this)
     }
 
-//    fun collecting(){
-//
-//    }
+    fun collecting(){
+        startBtn.setOnClickListener{
+            if ((UserIDfield.isFocused) and (UserIDfield.text.length != 0)){
+                Glide.with(this).asGif().load(R.drawable.gears2).into(imageView)
+                imageView.visibility = View.VISIBLE
+                textView2.text = "Collecting..."
+                startBtn.isEnabled = false;
+
+                Aware.startMagnetometer(this)
+
+                Timer().schedule(10000) {
+                    Aware.stopMagnetometer(applicationContext)
+                    println("Readings stopped!")
+                    runOnUiThread(java.lang.Runnable {
+                        Glide.with(applicationContext).asBitmap().load(R.drawable.done_icon).into(imageView)
+    //                    imageView.visibility = View.INVISIBLE
+                        textView2.text = "Data collected!"
+                        startBtn.isEnabled = true;
+                        done = true
+                    })
+                }
+
+                try {
+                    Magnetometer.getSensorObserver()
+                    Toast.makeText(this, "Pass", Toast.LENGTH_SHORT).show()
+                } catch (e: Exception) {
+                    Toast.makeText(this, "Crash", Toast.LENGTH_SHORT).show()
+                }
+            }
+            else{
+                Toast.makeText(this, "Please input some username first!", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
 }
 
 
---
+++ app/src/main/java/com/example/magnetometerdatacollection/PhaseActivity.kt
@@ -0,0 +1,57 @@
+package com.example.magnetometerdatacollection
+
+import android.Manifest
+import android.bluetooth.BluetoothAdapter
+import android.content.Context
+import android.content.pm.PackageManager
+import android.net.wifi.WifiManager
+import android.os.Bundle
+import android.provider.Settings
+import android.telephony.TelephonyManager
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.app.ActivityCompat
+
+
+@Suppress("DEPRECATION")
+class PhaseActivity : AppCompatActivity() {
+    lateinit var  textView: TextView
+    lateinit var button: Button
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_phase)
+        textView = findViewById(R.id.textView)
+        button = findViewById(R.id.button2)
+        textView.text = "Data collection is done in 5 stages. First you need to disable WiFi, Bluetooth, Mobile Data and make sure that your phone is not in Airpain mode." +
+                "Then please click the button below!"
+        button.setOnClickListener{
+            val wifi : WifiManager = getSystemService(Context.WIFI_SERVICE) as WifiManager
+            val tm = getSystemService(TELEPHONY_SERVICE) as TelephonyManager
+            val myBluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+            val AirPlainMode = Settings.System.getInt(contentResolver, Settings.Global.AIRPLANE_MODE_ON, 0) == 1
+
+            println("test button AirPlainMode == " + AirPlainMode)
+//            if (ActivityCompat.checkSelfPermission(
+//                    this,
+//                    Manifest.permission.READ_PHONE_STATE
+//                ) != PackageManager.PERMISSION_GRANTED
+//            ) {
+//                // TODO: Consider calling
+//                //    ActivityCompat#requestPermissions
+//                // here to request the missing permissions, and then overriding
+//                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+//                //                                          int[] grantResults)
+//                // to handle the case where the user grants the permission. See the documentation
+//                // for ActivityCompat#requestPermissions for more details.
+//                return@setOnClickListener
+//            }
+            if (wifi.isWifiEnabled and myBluetoothAdapter.isEnabled and tm.isDataEnabled) {
+                //TODO: Code to execute if wifi is enabled.
+                println("test button")
+            }
+            println("test button MOB DATA " + tm.isDataEnabled)
+        }
+
+    }
+}
